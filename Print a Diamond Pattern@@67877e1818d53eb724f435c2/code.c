739d78f30309f4348f2ff2652723e9b780806810
Devansh Negi <devansh0123.becse24@chitkara.edu.in>
2025-02-07 12:29:45 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -14,7 +14,7 @@ int main() {
                 }
             }
             else{
-                if((j < i - n + 1) || (j > 2 * n - i + 1)){
+                if((j < i - n + 1) || (j > 2 * n - i )){
                     printf(" ");
                 }
                 else{
